50212b6356ff10c20876f973a7628b3ef1af493b
Nadhif Ariq Danendra <nadhifariq180@gmail.com>
2023-06-07 12:45:16 +0700

    add n Nadhif

--- lib/ui/lowongan/chat_page/chat.dart
+++ lib/ui/lowongan/chat_page/chat.dart
@@ -46,60 +46,99 @@ class _ChatPageState extends State<ChatPage> {
             ),
           ),
         ),
-        body: Container(
-            padding: EdgeInsets.symmetric(horizontal: 15, vertical: 9),
-            child:
-                Column(crossAxisAlignment: CrossAxisAlignment.start, children: [
-              ListView.builder(
-                  itemCount: 2,
-                  shrinkWrap: true,
-                  itemBuilder: (context, index) {
-                    return Container(
-                        decoration: BoxDecoration(
-                            color: color.surfaceContainer,
-                            borderRadius: BorderRadius.circular(10)),
-                        width: double.maxFinite,
-                        height: 75,
-                        margin: EdgeInsets.symmetric(vertical: 10),
-                        padding: EdgeInsets.symmetric(horizontal: 10),
-                        child: Row(children: [
-                          CircleAvatar(
-                            radius: 30,
-                            backgroundImage: AssetImage(AppAssets.firdanImg),
-                          ),
-                          Expanded(
-                              child: Column(
-                                  crossAxisAlignment: CrossAxisAlignment.start,
-                                  children: [
-                                Container(
-                                  margin: EdgeInsets.only(left: 13),
-                                  padding: EdgeInsets.only(top: 15),
-                                  width: double.maxFinite,
-                                  child: Text(
-                                    'Milkita',
-                                    style: TextStyle(
-                                      fontSize: 20,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  ),
-                                ),
-                                Container(
-                                  margin: EdgeInsets.only(left: 13),
-                                  padding: EdgeInsets.only(top: 5),
-                                  width: double.maxFinite,
-                                  child: Text(
-                                    'Halo, saya ingin melamar pekerjaan',
-                                    style: TextStyle(
-                                      fontSize: 15,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  ),
-                                ),
-                              ]))
-                        ]));
-                  })
-            ]
-            )
-            ));
+        body: SingleChildScrollView(
+          child: Container(
+              padding: EdgeInsets.symmetric(horizontal: 15, vertical: 9),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    ListView.builder(
+                        itemCount: 2,
+                        shrinkWrap: true,
+                        itemBuilder: (context, index) {
+                          return Container(
+                              decoration: BoxDecoration(
+                                  color: color.surfaceContainer,
+                                  borderRadius: BorderRadius.circular(10)),
+                              width: double.maxFinite,
+                              height: 75,
+                              margin: EdgeInsets.symmetric(vertical: 10),
+                              padding: EdgeInsets.symmetric(horizontal: 10),
+                              child: Expanded(
+                                child: Row(
+                                    mainAxisAlignment:
+                                        MainAxisAlignment.spaceBetween,
+                                    children: [
+                                      CircleAvatar(
+                                        radius: 30,
+                                        backgroundImage:
+                                            AssetImage(AppAssets.firdanImg),
+                                      ),
+                                      Expanded(
+                                          child: Row(
+                                        mainAxisAlignment:
+                                            MainAxisAlignment.spaceBetween,
+                                        children: [
+                                          Column(
+                                              crossAxisAlignment:
+                                                  CrossAxisAlignment.start,
+                                              children: [
+                                                Container(
+                                                  margin:
+                                                      EdgeInsets.only(left: 13),
+                                                  padding:
+                                                      EdgeInsets.only(top: 15),
+                                                  child: Text(
+                                                    'Milkita',
+                                                    style: TextStyle(
+                                                      fontSize: 20,
+                                                      fontWeight:
+                                                          FontWeight.w400,
+                                                    ),
+                                                  ),
+                                                ),
+                                                Expanded(
+                                                  child: Row(
+                                                    crossAxisAlignment:
+                                                        CrossAxisAlignment
+                                                            .start,
+                                                    mainAxisAlignment:
+                                                        MainAxisAlignment
+                                                            .spaceBetween,
+                                                    children: [
+                                                      Container(
+                                                        margin: EdgeInsets.only(
+                                                            left: 7),
+                                                        //padding: EdgeInsets.only(top: 3),
+                                                        child: Text(
+                                                          'Halo, saya ingin melamar pekerjaan',
+                                                          style: TextStyle(
+                                                            fontSize: 15,
+                                                            fontWeight:
+                                                                FontWeight.w400,
+                                                          ),
+                                                        ),
+                                                      ),
+                                                    ],
+                                                  ),
+                                                ),
+                                              ]),
+                                          CircleAvatar(
+                                            child: Text(
+                                              "1",
+                                              style: TextStyle(
+                                                  color: Colors.white,
+                                                  fontSize: 10),
+                                            ),
+                                            radius: 10,
+                                            backgroundColor: color.primary,
+                                          )
+                                        ],
+                                      ))
+                                    ]),
+                              ));
+                        })
+                  ])),
+        ));
   }
 }
