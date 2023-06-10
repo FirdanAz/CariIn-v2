be724b9172afe63b1783e4924ae8bdd2843ed84d
unknown <firdannoobgam@gmail.com>
2023-06-10 07:14:50 +0700

    add profil sementara

--- lib/ui/lowongan/profile_page/Profile.dart
+++ lib/ui/lowongan/profile_page/Profile.dart
@@ -2,6 +2,8 @@ import 'dart:ui';
 import 'package:cariin_v2/common/app_assets.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 import 'package:sizer/sizer.dart';
 
 class ProfilePage extends StatefulWidget {
@@ -14,6 +16,12 @@ class ProfilePage extends StatefulWidget {
 class _ProfilePageState extends State<ProfilePage> {
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: Container(
+        width: double.maxFinite,
+        height: double.maxFinite,
+        child: SvgPicture.asset(AppAssets.sementara, fit: BoxFit.cover,),
+      ),
+    );
   }
 }
