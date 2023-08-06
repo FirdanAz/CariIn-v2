f611f52609548314c6ecdc321bb05fa7b34a3757
Nadhif Ariq Danendra <nadhifariq180@gmail.com>
2023-08-07 00:34:25 +0700

    style/ui_lowongan_melengkapi

---
+++ lib/ui/lowongan/profile_page/Detail_Profile_Pengalaman_DLL/Tambah_Pencapaian.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Tambah_PencapaianPage extends StatefulWidget {
+  const Tambah_PencapaianPage({Key? key}) : super(key: key);
+
+  @override
+  State<Tambah_PencapaianPage> createState() => _Tambah_PencapaianPageState();
+}
+
+class _Tambah_PencapaianPageState extends State<Tambah_PencapaianPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
