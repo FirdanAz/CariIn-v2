2e32f500b88722562bee2e01e9a10a5e40ef6cf0
unknown <firdannoobgam@gmail.com>
2023-08-14 10:37:42 +0700

    [MFA] add onwillpop worker

--- lib/ui/bottom_navigation/bottom_navigation.dart
+++ lib/ui/bottom_navigation/bottom_navigation.dart
@@ -1,5 +1,4 @@
 import 'package:cariin_v2/common/app_color.dart';
-import 'package:cariin_v2/model/worker/worker_profile_model.dart';
 import 'package:cariin_v2/ui/lowongan/chat_page/chat.dart';
 import 'package:cariin_v2/ui/lowongan/home_page/home.dart';
 import 'package:cariin_v2/ui/lowongan/lamar_page/lamaran.dart';
@@ -22,52 +21,75 @@ class _CustomBottomNavigationState extends State<CustomBottomNavigation> {
     const ProfilePage()
   ];
 
+  Future<bool> _onWillPop() async {
+    return (await showDialog(
+      context: context,
+      builder: (context) => AlertDialog(
+        title: const Text('Keluar Aplikasi'),
+        content: const Text('Yakin untuk keluar dari aplikasi?'),
+        actions: <Widget>[
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(false),
+            child: const Text('Batal'),
+          ),
+          TextButton(
+            onPressed: () => Navigator.of(context).pop(true),
+            child: const Text('Keluar'),
+          ),
+        ],
+      ),
+    )) ?? false;
+  }
+
   @override
   Widget build(BuildContext context) {
     var color = AppColor.theme(Theme.of(context).brightness);
 
-    return Scaffold(
-      body: _page[_pageIndex],
-      bottomNavigationBar: NavigationBar(
-        selectedIndex: _pageIndex,
-        height: 73,
-        destinations: [
-          NavigationDestination(
-              icon: Icon(
-                Icons.home_filled,
-                color: _pageIndex == 0
-                    ? color.white
-                    : Theme.of(context).iconTheme.color!.withOpacity(0.5),
-              ),
-              label: "Beranda"),
-          NavigationDestination(
-              icon: Icon(
-                Icons.work,
-                color: _pageIndex == 1
-                    ? color.white
-                    : Theme.of(context).iconTheme.color!.withOpacity(0.5),
-              ),
-              label: "Pekerjaan"),
-          NavigationDestination(
-              icon: Icon(
-                Icons.message,
-                color: _pageIndex == 2
-                    ? color.white
-                    : Theme.of(context).iconTheme.color!.withOpacity(0.5),
-              ),
-              label: "Pesan"),
-          NavigationDestination(
-              icon: Icon(
-                Icons.person,
-                color: _pageIndex == 3
-                    ? color.white
-                    : Theme.of(context).iconTheme.color!.withOpacity(0.5),
-              ),
-              label: "Profil"),
-        ],
-        onDestinationSelected: (value) => setState(() {
-          _pageIndex = value;
-        }),
+    return WillPopScope(
+      onWillPop: _onWillPop,
+      child: Scaffold(
+        body: _page[_pageIndex],
+        bottomNavigationBar: NavigationBar(
+          selectedIndex: _pageIndex,
+          height: 73,
+          destinations: [
+            NavigationDestination(
+                icon: Icon(
+                  Icons.home_filled,
+                  color: _pageIndex == 0
+                      ? color.white
+                      : Theme.of(context).iconTheme.color!.withOpacity(0.5),
+                ),
+                label: "Beranda"),
+            NavigationDestination(
+                icon: Icon(
+                  Icons.work,
+                  color: _pageIndex == 1
+                      ? color.white
+                      : Theme.of(context).iconTheme.color!.withOpacity(0.5),
+                ),
+                label: "Pekerjaan"),
+            NavigationDestination(
+                icon: Icon(
+                  Icons.message,
+                  color: _pageIndex == 2
+                      ? color.white
+                      : Theme.of(context).iconTheme.color!.withOpacity(0.5),
+                ),
+                label: "Pesan"),
+            NavigationDestination(
+                icon: Icon(
+                  Icons.person,
+                  color: _pageIndex == 3
+                      ? color.white
+                      : Theme.of(context).iconTheme.color!.withOpacity(0.5),
+                ),
+                label: "Profil"),
+          ],
+          onDestinationSelected: (value) => setState(() {
+            _pageIndex = value;
+          }),
+        ),
       ),
     );
   }
